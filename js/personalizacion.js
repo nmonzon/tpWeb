435292d75d3bf1ae715cb15546a9d7dd3b8823d5
Nicolas Monzon <nicolasmonzon0@gmail.com>
2019-07-01 17:49:48 -0300

    Agregado en personalizacion Cambio de fondo y de tamaño de fuente

--- js/personalizacion.js
+++ js/personalizacion.js
@@ -3,3 +3,51 @@ function changeNameEvent(){
     var nameEvent=document.getElementById('name-event');
     document.getElementById('newNameEvent').innerHTML= nameEvent.value.toUpperCase();
 }
+
+function changeColour(value)
+{
+    var background = document.body.style.backgroundColor;;
+    switch(value)
+    {
+        case 'b':
+            background = "lightskyblue";
+        break;
+        case 'r':
+            background = "crimson";
+        break;
+        case 'gr':
+            background = "lightslategrey";
+        break;
+        case 'g':
+            background = "forestgreen";
+        break;
+        case 'p':
+            background = "lightpink";
+        break;
+    }
+    document.getElementsByClassName("photoEvent")[0].style.backgroundColor = background;
+}
+
+function changeFontSize(value){
+    
+    var size = '20';
+    switch(value)
+    {
+        case '20':
+            size = "20px";
+        break;
+        case '28':
+            size = "28px";
+        break;
+        case '32':
+            size = "32px";
+        break;
+        case '48':
+            size = "48px";
+        break;
+        case '60':
+            size = "60px";
+        break;
+    }
+    document.getElementById("newNameEvent").style.fontSize = size;
+}
